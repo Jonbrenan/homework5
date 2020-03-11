260815d768149b9a3499593f16900b1927b71860
Jon Brenan <jonathanbrenan@gmail.com>
2020-03-11 13:29:23 -0400

    In this commit, I have added all the text blocks for the assignment. Saving now so if things get messed up in next step I will have this to go back to.
    Next step is getting the blocks to light up certain
    colors depending on what time of day it is

--- javascript.js
+++ javascript.js
@@ -2,35 +2,195 @@
 
 let contain = document.querySelector('.container')
 
+// creating attributes
 
 let nineAmDiv1 = $('<div>')
+let nineAmDiv2 = $('<div>')
+let nineAMspan = $('<span>')
+let nineAmTextArea = $('<textarea>')
+let nineAmButton = $('<button>')
+
+let tenAmDiv1 = $('<div>')
+let tenAmDiv2 = $('<div>')
+let tenAMspan = $('<span>')
+let tenAmTextArea = $('<textarea>')
+let tenAmButton = $('<button>')
+
+let elevenAmDiv1 = $('<div>')
+let elevenAmDiv2 = $('<div>')
+let elevenAMspan = $('<span>')
+let elevenAmTextArea = $('<textarea>')
+let elevenAmButton = $('<button>')
+
+let twelvePmDiv1 = $('<div>')
+let twelvePmDiv2 = $('<div>')
+let twelvePmspan = $('<span>')
+let twelvePmTextArea = $('<textarea>')
+let twelvePmButton = $('<button>')
+
+let onePmDiv1 = $('<div>')
+let onePmDiv2 = $('<div>')
+let onePmspan = $('<span>')
+let onePmTextArea = $('<textarea>')
+let onePmButton = $('<button>')
+
+
+let twoPmDiv1 = $('<div>')
+let twoPmDiv2 = $('<div>')
+let twoPmspan = $('<span>')
+let twoPmTextArea = $('<textarea>')
+let twoPmButton = $('<button>')
+
+
+let threePmDiv1 = $('<div>')
+let threePmDiv2 = $('<div>')
+let threePmspan = $('<span>')
+let threePmTextArea = $('<textarea>')
+let threePmButton = $('<button>')
+
+
+let fourPmDiv1 = $('<div>')
+let fourPmDiv2 = $('<div>')
+let fourPmspan = $('<span>')
+let fourPmTextArea = $('<textarea>')
+let fourPmButton = $('<button>')
+
+
+let fivePmDiv1 = $('<div>')
+let fivePmDiv2 = $('<div>')
+let fivePmspan = $('<span>')
+let fivePmTextArea = $('<textarea>')
+let fivePmButton = $('<button>')
+
+
+
+
+
+//attributes
+
 
 nineAmDiv1.attr('class', 'input-group')
 nineAmDiv1.attr("id", 'nineAmDiv1')
-
-let nineAmDiv2 = $('<div>')
-
 nineAmDiv2.attr('class', 'input-group-prepend')
 nineAmDiv2.attr('id', 'nineAmDiv2')
-
-let nineAMspan = $('<span>')
-
 nineAMspan.attr('class', 'input-group-text')
-
 nineAMspan.text('9am')
-
-let nineAmTextArea = $('<textarea>')
-
 nineAmTextArea.attr('class', 'form-control')
 nineAmTextArea.attr('aria-label', 'With textarea')
-
-let nineAmButton = $('<button>')
-
 nineAmButton.attr('class', 'btn btn-outline-secondary')
 nineAmButton.attr('type', 'button')
 nineAmButton.attr('id', 'button-addon2')
 nineAmButton.text('Save')
 
+tenAmDiv1.attr('class', 'input-group')
+tenAmDiv1.attr("id", 'tenAmDiv1')
+tenAmDiv2.attr('class', 'input-group-prepend')
+tenAmDiv2.attr('id', 'tenAmDiv2')
+tenAMspan.attr('class', 'input-group-text')
+tenAMspan.text('10am')
+tenAmTextArea.attr('class', 'form-control')
+tenAmTextArea.attr('aria-label', 'With textarea')
+tenAmButton.attr('class', 'btn btn-outline-secondary')
+tenAmButton.attr('type', 'button')
+tenAmButton.attr('id', 'button-addon2')
+tenAmButton.text('Save')
+
+elevenAmDiv1.attr('class', 'input-group')
+elevenAmDiv1.attr("id", 'elevenAmDiv1')
+elevenAmDiv2.attr('class', 'input-group-prepend')
+elevenAmDiv2.attr('id', 'elevenAmDiv2')
+elevenAMspan.attr('class', 'input-group-text')
+elevenAMspan.text('11am')
+elevenAmTextArea.attr('class', 'form-control')
+elevenAmTextArea.attr('aria-label', 'With textarea')
+elevenAmButton.attr('class', 'btn btn-outline-secondary')
+elevenAmButton.attr('type', 'button')
+elevenAmButton.attr('id', 'button-addon2')
+elevenAmButton.text('Save')
+
+twelvePmDiv1.attr('class', 'input-group')
+twelvePmDiv1.attr("id", 'twelvePmDiv1')
+twelvePmDiv2.attr('class', 'input-group-prepend')
+twelvePmDiv2.attr('id', 'twelvePmDiv2')
+twelvePmspan.attr('class', 'input-group-text')
+twelvePmspan.text('12pm')
+twelvePmTextArea.attr('class', 'form-control')
+twelvePmTextArea.attr('aria-label', 'With textarea')
+twelvePmButton.attr('class', 'btn btn-outline-secondary')
+twelvePmButton.attr('type', 'button')
+twelvePmButton.attr('id', 'button-addon2')
+twelvePmButton.text('Save')
+
+
+onePmDiv1.attr('class', 'input-group')
+onePmDiv1.attr("id", 'onePmDiv1')
+onePmDiv2.attr('class', 'input-group-prepend')
+onePmDiv2.attr('id', 'onePmDiv2')
+onePmspan.attr('class', 'input-group-text')
+onePmspan.text('1pm')
+onePmTextArea.attr('class', 'form-control')
+onePmTextArea.attr('aria-label', 'With textarea')
+onePmButton.attr('class', 'btn btn-outline-secondary')
+onePmButton.attr('type', 'button')
+onePmButton.attr('id', 'button-addon2')
+onePmButton.text('Save')
+
+twoPmDiv1.attr('class', 'input-group')
+twoPmDiv1.attr("id", 'twoPmDiv1')
+twoPmDiv2.attr('class', 'input-group-prepend')
+twoPmDiv2.attr('id', 'twoPmDiv2')
+twoPmspan.attr('class', 'input-group-text')
+twoPmspan.text('2pm')
+twoPmTextArea.attr('class', 'form-control')
+twoPmTextArea.attr('aria-label', 'With textarea')
+twoPmButton.attr('class', 'btn btn-outline-secondary')
+twoPmButton.attr('type', 'button')
+twoPmButton.attr('id', 'button-addon2')
+twoPmButton.text('Save')
+
+
+threePmDiv1.attr('class', 'input-group')
+threePmDiv1.attr("id", 'threePmDiv1')
+threePmDiv2.attr('class', 'input-group-prepend')
+threePmDiv2.attr('id', 'threePmDiv2')
+threePmspan.attr('class', 'input-group-text')
+threePmspan.text('3pm')
+threePmTextArea.attr('class', 'form-control')
+threePmTextArea.attr('aria-label', 'With textarea')
+threePmButton.attr('class', 'btn btn-outline-secondary')
+threePmButton.attr('type', 'button')
+threePmButton.attr('id', 'button-addon2')
+threePmButton.text('Save')
+
+
+fourPmDiv1.attr('class', 'input-group')
+fourPmDiv1.attr("id", 'fourPmDiv1')
+fourPmDiv2.attr('class', 'input-group-prepend')
+fourPmDiv2.attr('id', 'fourPmDiv2')
+fourPmspan.attr('class', 'input-group-text')
+fourPmspan.text('4pm')
+fourPmTextArea.attr('class', 'form-control')
+fourPmTextArea.attr('aria-label', 'With textarea')
+fourPmButton.attr('class', 'btn btn-outline-secondary')
+fourPmButton.attr('type', 'button')
+fourPmButton.attr('id', 'button-addon2')
+fourPmButton.text('Save')
+
+
+fivePmDiv1.attr('class', 'input-group')
+fivePmDiv1.attr("id", 'fivePmDiv1')
+fivePmDiv2.attr('class', 'input-group-prepend')
+fivePmDiv2.attr('id', 'fivePmDiv2')
+fivePmspan.attr('class', 'input-group-text')
+fivePmspan.text('5pm')
+fivePmTextArea.attr('class', 'form-control')
+fivePmTextArea.attr('aria-label', 'With textarea')
+fivePmButton.attr('class', 'btn btn-outline-secondary')
+fivePmButton.attr('type', 'button')
+fivePmButton.attr('id', 'button-addon2')
+fivePmButton.text('Save')
+
+//appends
 
 $('.container').append(nineAmDiv1)
 
@@ -43,4 +203,89 @@ $('#nineAmDiv1').append(nineAmTextArea)
 $('#nineAmDiv1').append(nineAmButton)
 
 
+$('.container').append(tenAmDiv1)
 
+$('#tenAmDiv1').append(tenAmDiv2)
+
+$('#tenAmDiv2').append(tenAMspan)
+
+$('#tenAmDiv1').append(tenAmTextArea)
+
+$('#tenAmDiv1').append(tenAmButton)
+
+
+$('.container').append(elevenAmDiv1)
+
+$('#elevenAmDiv1').append(elevenAmDiv2)
+
+$('#elevenAmDiv2').append(elevenAMspan)
+
+$('#elevenAmDiv1').append(elevenAmTextArea)
+
+$('#elevenAmDiv1').append(elevenAmButton)
+
+
+$('.container').append(twelvePmDiv1)
+
+$('#twelvePmDiv1').append(twelvePmDiv2)
+
+$('#twelvePmDiv2').append(twelvePmspan)
+
+$('#twelvePmDiv1').append(twelvePmTextArea)
+
+$('#twelvePmDiv1').append(twelvePmButton)
+
+
+$('.container').append(onePmDiv1)
+
+$('#onePmDiv1').append(onePmDiv2)
+
+$('#onePmDiv2').append(onePmspan)
+
+$('#onePmDiv1').append(onePmTextArea)
+
+$('#onePmDiv1').append(onePmButton)
+
+
+$('.container').append(twoPmDiv1)
+
+$('#twoPmDiv1').append(twoPmDiv2)
+
+$('#twoPmDiv2').append(twoPmspan)
+
+$('#twoPmDiv1').append(twoPmTextArea)
+
+$('#twoPmDiv1').append(twoPmButton)
+
+
+$('.container').append(threePmDiv1)
+
+$('#threePmDiv1').append(threePmDiv2)
+
+$('#threePmDiv2').append(threePmspan)
+
+$('#threePmDiv1').append(threePmTextArea)
+
+$('#threePmDiv1').append(threePmButton)
+
+
+$('.container').append(fourPmDiv1)
+
+$('#fourPmDiv1').append(fourPmDiv2)
+
+$('#fourPmDiv2').append(fourPmspan)
+
+$('#fourPmDiv1').append(fourPmTextArea)
+
+$('#fourPmDiv1').append(fourPmButton)
+
+
+$('.container').append(fivePmDiv1)
+
+$('#fivePmDiv1').append(fivePmDiv2)
+
+$('#fivePmDiv2').append(fivePmspan)
+
+$('#fivePmDiv1').append(fivePmTextArea)
+
+$('#fivePmDiv1').append(fivePmButton)
